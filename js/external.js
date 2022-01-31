1a9177b475d58437c4697918da6f7f8eadd0330d
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-01-31 16:37:47 -0600

    Take off comments, so all exercises can run

--- js/external.js
+++ js/external.js
@@ -7,49 +7,49 @@ console.log("Hello from EXTERNAL JavaScript.")
 // //     Use an alert to show the results of each problem.
 //
 // // Use the alert function to show a message that says 'Welcome to my Website!'.
-//  alert('Welcome to my Website!');
+ alert('Welcome to my Website!');
 //
 // //Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the color entered is your favorite color too.
 // //For example, if the user enters "blue", your code should alert a message that says:
-// const favColor = prompt("What's your favorite color?");
-// alert("Wow, I didn't know your favorite color is " + favColor + ".");
+const favColor = prompt("What's your favorite color?");
+alert("Wow, I didn't know your favorite color is " + favColor + ".");
 //
 //
 //
 // //------  Button Exercise
-// const toggleButton = document.getElementById('toggle');
-// const hideParagraph = document.getElementById('hideable');
-//
-// toggleButton.onclick = function() {
-//     let theClass = hideParagraph.getAttribute("class");
-//     if( theClass == "hidden"){
-//         hideParagraph.removeAttribute("class");
-//         alert('YEEEHHAAA ITS BACK!');
-//     } else {
-//         hideParagraph.setAttribute('class', "hidden");
-//         alert('OH NO! You took out the paragraph!');
-//     }
-// }
+const toggleButton = document.getElementById('toggle');
+const hideParagraph = document.getElementById('hideable');
+
+toggleButton.onclick = function() {
+    let theClass = hideParagraph.getAttribute("class");
+    if( theClass == "hidden"){
+        hideParagraph.removeAttribute("class");
+        alert('YEEEHHAAA ITS BACK!');
+    } else {
+        hideParagraph.setAttribute('class', "hidden");
+        alert('OH NO! You took out the paragraph!');
+    }
+}
 //
 //
 //
 // //----- Exercise Dataytype page.
-// alert('Exercise #2');
-// // the password must not include the username
-// // neither the username or password can start or end with whitespace
-// var username = 'codeup';
-// var password = 'notastrongpassword';
-//
-// let containerHoldingBooleanValue = (password.length >= 5 && username.length < 20);
-// let passwordUsername = (-1 == password.search(username));     // One way to do it
-// passwordUsername = !password.includes(username);              // Another way to do it
-// let usernameNew = !username.endsWith(" ") && !password.endsWith(' ') && !username.startsWith(" ") && !password.startsWith(" ")
-// !password.endsWith(' ') ;
-//
-// if (containerHoldingBooleanValue && passwordUsername && usernameNew){
-//     alert('your password and username works!');
-// }
-//
+alert('Exercise #2');
+// the password must not include the username
+// neither the username or password can start or end with whitespace
+var username = 'codeup';
+var password = 'notastrongpassword';
+
+let containerHoldingBooleanValue = (password.length >= 5 && username.length < 20);
+let passwordUsername = (-1 == password.search(username));     // One way to do it
+passwordUsername = !password.includes(username);              // Another way to do it
+let usernameNew = !username.endsWith(" ") && !password.endsWith(' ') && !username.startsWith(" ") && !password.startsWith(" ")
+!password.endsWith(' ') ;
+
+if (containerHoldingBooleanValue && passwordUsername && usernameNew){
+    alert('your password and username works!');
+}
+
 
 
 
