99deb10b21346eeddcf8d67550a0d6a4e7b47066
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-07 13:34:04 -0600

    added a bonus while loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,12 +3,7 @@ while(numberZero < 65536){
     numberZero = numberZero * 2;
     console.log(numberZero);
 }
-var stars = '*';
-while (numberZero < 100){
 
-    console.log("1");
-    numberZero++;
-}
 
 //---- Practice Exercise Personal
 var x = 1;
