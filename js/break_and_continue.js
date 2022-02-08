043d4a794f75c4e3884dd61e7ee46d44d5c07c01
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-08 12:04:32 -0600

    Completed excersise

---
+++ js/break_and_continue.js
@@ -0,0 +1,11 @@
+var promptInput = prompt('Please insert an ODD number between 1 - 50;');
+document.write('Number to skip is: #'+promptInput+'<br>')
+for(var i= 1;i<50;i+=2){
+    if (promptInput == i){
+        document.write("Yikes! it is your Number " + i+ '<br>');
+        continue;
+    } else{
+        document.write(i +'<br>')
+    }
+}
+
