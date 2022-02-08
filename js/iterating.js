c3ae7fdf8a33c180714ec36fad2887df810b633d
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-08 15:05:41 -0600

    exercise 1 completed

--- js/iterating.js
+++ js/iterating.js
@@ -6,7 +6,7 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    const names = ['josh', 'heather', 'parker', 'smith'];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
