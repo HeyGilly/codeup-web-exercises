269c9d7799cb339c1f0fc9f75e1afdadda53bbf7
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-08 09:31:28 -0600

    practice morning functions- completed

--- Moring Exercise/MoringExercise.js
+++ Moring Exercise/MoringExercise.js
@@ -19,22 +19,66 @@
 
 
 
-//------- Jan 31, 22- Monday
+//------- Feb 08 - Monday
 // write a function that takes in an input and returns the string "I have no idea what you just said";
-function whatHappen(takesInput){
-    return "I have no idea what you just said";
+// function whatHappen(takesInput){
+//     return "I have no idea what you just said";
+// };
+// console.log(whatHappen('this is new input'));
+//
+// //anonymous Function - Expression
+// let whatYouSay = function(input){
+//     alert("no you didnt");
+// };
+// whatYouSay("wow");
+//
+// //anonymous Function - Declaration
+// (function(){
+//     alert("I have no idea what is going on");
+// })();
+//
+//
+
+//------- Feb 09, 22- Tuesday
+//write a function that in a number and returns that number plus itself.
+function moringExercise(numInput) {
+    return Number(numInput) + Number(numInput);
 };
-console.log(whatHappen('this is new input'));
+console.log(moringExercise(2));
 
-//anonymous Function - Expression
-let whatYouSay = function(input){
-    alert("no you didnt");
-};
-whatYouSay("wow");
+//Write a function that takes in two numbers, doubles each number, and then returns the sum of both numbers doubled.
+function twoNumbers(num1, num2){
+    return (num1*2)+(num2*2);
+}
 
-//anonymous Function - Declaration
-(function(){
-    alert("I have no idea what is going on");
-})();
+console.log(twoNumbers(1, 2));
 
+//write a function that takes in a number and squares itself.
+function square(squareThisNum){
+    return squareThisNum ** 2;
+}
 
+console.log(square(5));
+
+//write a function that takes in a string and returns its length
+function lengthString(x){
+    return x.length;
+}
+
+console.log(lengthString("hello dsfasdf"));
+
+//write a fucntion that takes in two strings and return the sum of their lengths.
+function sumLength(str1, str2){
+    return str1.length + str2.length;
+}
+
+console.log(sumLength('why', 'who I cant belaive it '));
+
+// write a function that take in a number and return the sstring even or off depending on where thae number is even
+// or odd.
+
+function evenOdd(numInput){
+    return (numInput % 2 == 0) ? 'even':'odd';
+}
+
+console.log(evenOdd(7));
