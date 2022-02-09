175c1d0dff90091ea0cc98df18326e8f18e3f8e0
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-09 09:56:47 -0600

    finished exercise

--- js/planets-array.js
+++ js/planets-array.js
@@ -23,19 +23,25 @@
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+    planets.shift();
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
+    planets.pop();
     console.log(planets);
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
+    console.log(planets[2]);;
 
     console.log("Reversing the order of the planets array.");
+    planets.reverse();
     console.log(planets);
 
     console.log("Sorting the planets array.");
+    planets.sort();
     console.log(planets);
 })();
