6539db726a6ea194fe728cc362d8a5b9f484b211
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-16 10:38:13 -0600

    added back the exercise that I accedently took off, While Loop was still removed

--- js/for_loops.js
+++ js/for_loops.js
@@ -15,6 +15,31 @@ while (x <= 4) {
 
 
 
+
+
+document.write( '--------------------- <br>')
+//--For Loop 2
+function showMultiplicationTable(acceptsNumbers){
+    for (let i = 1 ; i <= 10;i++){
+        document.write(acceptsNumbers + ' x '+ i + ' = '+(acceptsNumbers*i)+ '<br>');
+    }
+}
+
+console.log(showMultiplicationTable(5));
+
+
+document.write( '--------------------- <br>')
+//--For Loop 3
+//random number
+for (let x =1;x<10;x++){
+    var randomNumber = Math.floor(Math.random() * 200) + 20;
+    if (randomNumber % 2 == 0){
+        document.write( randomNumber+" is even!" + '<br>')
+    }else{
+        document.write( randomNumber+" is Odd!" + '<br>')
+    }
+}
+
 document.write( '--------------------- <br>')
 //--ForLoop 4
 //Create a for loop that uses console.log to create the output shown below.
@@ -35,6 +60,7 @@ for (let wow=100;wow>0;wow-=5){
 }
 
 
+//--- More practice with for loops
 
 
 
