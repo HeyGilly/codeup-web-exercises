eb46e18eaa0788bced0cbd452b293b62c93cd297
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-02-16 10:24:20 -0600

    feb1622 morning exercise

--- Moring Exercise/MoringExercise.js
+++ Moring Exercise/MoringExercise.js
@@ -88,13 +88,17 @@
 //------- Feb 16, 22- Tuesday
 const areaOrPerimeter = function(l,w){
     //Return your answer
+    if(isNaN(parseFloat(l)) || isNaN(parseFloat(w))) {
+        return false;
+    }
     var num = 0
     if (l === w){
         num = 4 * l;
     }else {
-        num = 2*(l*w);
+        num = 2*l + 2*w;
     }
     return num
 }
 console.log(areaOrPerimeter(4, 4), 16);
-console.log(areaOrPerimeter(6, 10), 120);
+console.log(areaOrPerimeter(6, 10), 32);
+console.log(areaOrPerimeter("hello", 10), false);
