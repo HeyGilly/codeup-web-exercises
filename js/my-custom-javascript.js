59dec17ba1413f978aa3d16e1bfe749729dac5f4
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-03-08 14:24:28 -0600

    When you click on the header it changes the style of the header

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -6,18 +6,40 @@ $(document).ready(function() {
 
 
 
+    $('.codeup').removeClass('codeup');
+
+    $('li').css({
+        "font-size":"20px",
+        "letter-spacing":"1px"
+    })
 
 
+    $('header').css({
+        "font-size":"40px",
+        "text-align":"center",
+        "height":"auto",
+        "background-color":"grey",
+        "width":"100vw",
+        "letter-spacing":"1px"
+    })
 
-
-    alert( 'The DOM has finished loading!' );
-
-
-
-
-
-
-
+    //ON click with the HEADER
+    $('header').click(function (e){
+        $(this).css({
+            "font-size":"40px",
+            "text-align":"center",
+            'padding': "40px 0",
+            'text-transform': "uppercase",
+            "height":"fit-content",
+            "background-color":"royalblue",
+            "width":"100vw",
+            "letter-spacing":"1px",
+            'color':'white'
+        });
+        $(this).html(
+            '<h1 class="container">Welcome to my new and Improved Page!</h1>'
+        )
+    })
 
 
 });
